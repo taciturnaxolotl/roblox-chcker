e0aa90fb83b1671b2e0b9d5c08f5e4e51a237346
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-11-02 17:00:17 -0400

    fix: add error handling for username validation process

--- index.ts
+++ index.ts
@@ -103,49 +103,55 @@ const results: {
 	err?: string;
 }[] = [];
 
-for (const name of names) {
-	// check if Usernames may only contain letters, numbers, and _.
-	// or Usernames can be 3 to 20 characters long.
-	// or Usernames can't contain spaces.
-	// or usernames can't contain more than one total underscore
+try {
+	for (const name of names) {
+		// check if Usernames may only contain letters, numbers, and _.
+		// or Usernames can be 3 to 20 characters long.
+		// or Usernames can't contain spaces.
+		// or usernames can't contain more than one total underscore
 
-	if (name.length < 3) {
-		s.message(`${name} is too short`);
-		results.push({ name, valid: false, err: "too short" });
-		continue;
-	}
-	if (name.length > 20) {
-		s.message(`${name} is too long`);
-		results.push({ name, valid: false, err: "too long" });
-		continue;
-	}
-	if (name.includes(" ")) {
-		s.message(`${name} contains a space`);
-		results.push({ name, valid: false, err: "contains a space" });
-		continue;
-	}
-	if (name.includes("_")) {
-		const underscoreCount = name.split("_").length - 1;
-		if (underscoreCount > 1) {
-			s.message(`${name} contains more than one underscore`);
-			results.push({
-				name,
-				valid: false,
-				err: "contains more than one underscore",
-			});
+		if (name.length < 3) {
+			s.message(`${name} is too short`);
+			results.push({ name, valid: false, err: "too short" });
 			continue;
 		}
+		if (name.length > 20) {
+			s.message(`${name} is too long`);
+			results.push({ name, valid: false, err: "too long" });
+			continue;
+		}
+		if (name.includes(" ")) {
+			s.message(`${name} contains a space`);
+			results.push({ name, valid: false, err: "contains a space" });
+			continue;
+		}
+		if (name.includes("_")) {
+			const underscoreCount = name.split("_").length - 1;
+			if (underscoreCount > 1) {
+				s.message(`${name} contains more than one underscore`);
+				results.push({
+					name,
+					valid: false,
+					err: "contains more than one underscore",
+				});
+				continue;
+			}
+		}
+
+		const { valid, err } = await validateUsername(browser, name);
+
+		if (!valid) {
+			s.message(`${name} isn't valid: ${err}`);
+		} else {
+			s.message(`${name} is valid`);
+		}
+
+		results.push({ name, valid, err });
 	}
-
-	const { valid, err } = await validateUsername(browser, name);
-
-	if (!valid) {
-		s.message(`${name} isn't valid: ${err}`);
-	} else {
-		s.message(`${name} is valid`);
+} catch (err) {
+	if (err instanceof Error) {
+		log.error(err.message);
 	}
-
-	results.push({ name, valid, err });
 }
 
 s.stop(
